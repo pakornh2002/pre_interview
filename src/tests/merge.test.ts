d5adf16ca85565d481e477e63dc9b5884ec66228
Pakorn Skunsongbunsiri <pakornh2002@gmail.com>
2024-06-04 21:59:25 +0700

    Add comment

--- src/tests/merge.test.ts
+++ src/tests/merge.test.ts
@@ -1,13 +1,13 @@
-import { merge } from "../merge"
+import { merge } from "../merge" //import merge function from merge.ts
 
-describe('merge function', () =>{
+describe('merge function', () =>{ //write unit test for merge function
     it('should merge three sorted arrays and return a sorted array', () =>{
-        const collection1 = [1,3,6,7,13]
-        const collection2 = [11,9,8,5,2]
-        const collection3 = [4,6,8,10,12]
+        const collection1 = [1,3,6,7,13] //given collection1 as array of number sorted by ASC
+        const collection2 = [11,9,8,5,2] //given collection2 as array of number sorted by DES
+        const collection3 = [4,6,8,10,12] //given collection3 as array of number sorted by ASC
 
-        const result = merge(collection1,collection2,collection3)
+        const result = merge(collection1,collection2,collection3) //given result as a result from merge function
 
-        expect(result).toEqual([1,2,3,4,5,6,6,7,8,8,9,10,11,12,13])
+        expect(result).toEqual([1,2,3,4,5,6,6,7,8,8,9,10,11,12,13]) //expect result to equal to assumed output
     })
 })
