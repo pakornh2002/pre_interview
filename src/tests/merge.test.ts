448b5d296183eac11aae5c8d43bad47f3f24f75f
Pakorn Skunsongbunsiri <pakornh2002@gmail.com>
2024-06-05 08:07:46 +0700

    Change testcase in merge.test.ts

--- src/tests/merge.test.ts
+++ src/tests/merge.test.ts
@@ -4,10 +4,10 @@ describe('merge function', () =>{ //write unit test for merge function
     it('should merge three sorted arrays and return a sorted array', () =>{
         const collection1 = [1,3,6,7,9] //given collection1 as array of number sorted by ASC
         const collection2 = [10,9,8,5,2] //given collection2 as array of number sorted by DES
-        const collection3 = [4,6,8,11,12] //given collection3 as array of number sorted by ASC
+        const collection3 = [1,2,4,11,12] //given collection3 as array of number sorted by ASC
 
         const result = merge(collection1,collection2,collection3) //given result as a result from merge function
 
-        expect(result).toEqual([1,2,3,4,5,6,6,7,8,8,9,9,10,11,12]) //expect result to equal to assumed output
+        expect(result).toEqual([1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11, 12]) //expect result to equal to assumed output
     })
 })
